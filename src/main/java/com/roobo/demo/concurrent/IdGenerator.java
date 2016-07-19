23082457fa6e398ed94c2543ecd82cfa07aabf96
唐琦 <tangqi@roo.bo>
2016-07-19 19:55:45 +0800

    增加练习类

---
+++ src/main/java/com/roobo/demo/concurrent/IdGenerator.java
@@ -0,0 +1,23 @@
+package com.roobo.demo.concurrent;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ ID序列生成器
+*/
+public class IdGenerator {
+
+    private final AtomicLong sequenceNumber = new AtomicLong(0);
+
+    public long next() {
+        return sequenceNumber.addAndGet(1);
+    }
+
+
+    public static void main(String[] args) {
+        IdGenerator idGenerator1 = new IdGenerator();
+        System.out.println(idGenerator1.next());
+        IdGenerator idGenerator2 = new IdGenerator();
+        System.out.println(idGenerator2.next());
+    }
+}
