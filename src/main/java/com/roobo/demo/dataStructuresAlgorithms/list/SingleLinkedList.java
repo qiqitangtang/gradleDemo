3471ba373c54a1714aec4a63f8b04b196dfdc7f6
唐琦 <tangqi@huijukeji.cn>
2017-03-25 10:42:35 +0800

    增加链表面试题方法

--- src/main/java/com/roobo/demo/dataStructuresAlgorithms/list/SingleLinkedList.java
+++ src/main/java/com/roobo/demo/dataStructuresAlgorithms/list/SingleLinkedList.java
@@ -1,5 +1,6 @@
 package com.roobo.demo.dataStructuresAlgorithms.list;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 /**
@@ -12,7 +13,8 @@ public class SingleLinkedList {
      * @return
      */
     public static Node createLinkedList(){
-        Node left9 = new Node(10,null);
+        Node root = new Node(1,null);
+        Node left9 = new Node(10,root);
         Node left8  = new Node(9,left9);
         Node left7  = new Node(8,left8);
         Node left6  = new Node(7,left7);
@@ -21,7 +23,7 @@ public class SingleLinkedList {
         Node left3  = new Node(4,left4);
         Node left2  = new Node(3,left3);
         Node left1  = new Node(2,left2);
-        Node root = new Node(1,left1);
+        root.setNext(left1);
         return root;
     }
 
@@ -125,7 +127,7 @@ public class SingleLinkedList {
      * 获取中间节点（两个指针，一个一次一个节点，一个一次两个节点）
      * @param head
      */
-    public static void reGetMiddleNode(Node head){
+    public static Node reGetMiddleNode(Node head){
         Node p1 = head,p2 = head;
         while(p2.getNext() != null){
             p1 = p1.getNext();
@@ -135,6 +137,7 @@ public class SingleLinkedList {
             }
         }
         System.out.println("链表中间节点为：" + p1.getValue());
+        return p1;
     }
 
     /**
@@ -169,29 +172,212 @@ public class SingleLinkedList {
      * 合并两个有序的单链表head1和head2，循环
      * @param head1,head2
      */
-    public static void mergeSortedList(Node head1,Node head2){
-        if(head1 == null || head2 == null ||head1.getNext() == null) return;
-        while(head1.getNext() != null){
-            if(head1.getValue() <= head2.getValue() && head2.getValue() < head1.getNext().getValue()){
-                Node n = head2;
-                head2 = n.getNext();
-                n.setNext(head1.getNext());
-                head1.setNext(n);
+    public static Node mergeSortedList(Node head1,Node head2){
+        if (head1 == null) printList(head2);
+        if (head2 == null) printList(head1);
+        Node mergeHead = null;
+        if(head1.getValue() <= head2.getValue()){
+          mergeHead = head1;
+          head1 = head1.getNext();
+        }else{
+          mergeHead = head2;
+          head2 = head2.getNext();
+        }
+        mergeHead.setNext(null);
+        Node temp = mergeHead;
+        while(head1 != null && head2 != null){
+            if(head1.getValue() <= head2.getValue()){
+                temp.setNext(head1);
+                head1 = head1.getNext();
+
+            }else{
+                temp.setNext(head2);
+                head2 = head2.getNext();
             }
-            head1 = head1.getNext();
+            temp = temp.getNext();
+            temp.setNext(null);
+        }
+        if(head1 != null){
+            temp.setNext(head1);
+        }
+        if(head2 != null){
+            temp.setNext(head2);
         }
 
-
+        //printList(mergeHead);
+        return mergeHead;
     }
 
     /**
      * 合并两个有序的单链表head1和head2，递归
      * @param head1,head2
      */
-    public static void mergeSortedListRec(Node head1,Node head2){
+    public static Node mergeSortedListRec(Node head1,Node head2){
+        if (head1 == null) return head2;
+        if (head2 == null) return head1;
+        Node mergeHead = null;
+        if(head1.getValue() > head2.getValue()){
+            mergeHead = head2;
+            mergeHead.setNext(mergeSortedListRec(head1,head2.getNext()));
+        }else{
+            mergeHead = head1;
+            mergeHead.setNext(mergeSortedListRec(head1.getNext(),head2));
+        }
+        return mergeHead;
+    }
+
+    /**
+     * 对单链表进行排序 归并
+     * @param head
+     */
+    public static Node listSort(Node head){
+        Node next = null;
+        if(head == null || head.getNext() == null) return head;
+        else if (head.getNext().getNext() == null){
+            next = head.getNext();
+            head.setNext(null);
+        }else{
+            Node mid = reGetMiddleNode(head);
+            next = mid.getNext();
+            mid.setNext(null);
+        }
+        return mergeSortedList(listSort(head),listSort(next));
+    }
+
+    /**
+     * 对单链表进行排序 插入
+     * @param head
+     */
+    public static Node insertionSortList(Node head){
+        return null;
+    }
+
+    /**
+     * 判断链表中是否有环：一个指针是另一个指针的2倍，相遇即有环
+     * @param head
+     */
+    public static void hasCycle(Node head){
+        boolean isHasCycle = false;
+        Node p1 = head;
+        Node p2 = head;
+        while (p1 != null && p2 != null){
+            p1 = p1.getNext();
+            p2 = p2.getNext().getNext();
+            if(p1 == p2){
+                isHasCycle = true;
+                break;
+            }
+        }
+        System.out.println("链表中是否有环:" + isHasCycle);
+    }
+
+    /**
+     * 判断两个单链表是否相交,如果相交返回第一个节点，否则返回null
+     * 如果单纯的判断是否相交，只需要看最后一个指针是否相等
+     * @param head1,head2
+     */
+    public static Node isIntersect(Node head1,Node head2){
+        Node lastNode = null;
+        boolean isIntersect = false;
+        if(head1 == null || head2 == null ) return lastNode;
+        int len1 = getListLength(head1);
+        int len2 = getListLength(head2);
+        if(len1 >= len2){
+            for(int i=0 ;i<len1-len2;i++){
+                head1 = head1.getNext();
+            }
+        }else{
+            for(int i=0 ;i<len2-len1;i++){
+                head2 = head2.getNext();
+            }
+        }
+
+        while(head1 != null && head2 != null){
+            if(head1 == head2){
+                lastNode = head1;
+                isIntersect = true;
+                break;
+            }else{
+                head1 = head1.getNext();
+                head2 = head2.getNext();
+            }
+        }
+
+        if(isIntersect){
+            System.out.println("两个链表相交的第一个节点为：" + lastNode.getValue());
+        }
+        return lastNode;
+    }
+
+    /**
+     * 判断是否有环，求进入环的第一个节点 hashmap 去重复，第一个重复 的就是
+     * @param head
+     * @return
+     */
+    public static Node getFirstNodeInCycleHashMap(Node head){
+        HashMap<Node,Boolean> map = new HashMap<Node,Boolean>();
+        Node target = null;
+        while (head != null){
+            if(map.containsKey(head)) {
+                target = head;
+                break;
+            }else{
+                map.put(head,true);
+            }
+            head = head.getNext();
+        }
+        return target;
+    }
+
+    /**
+     * 用快慢指针，与判断一个单链表中是否有环一样，
+     * 找到快慢指针第一次相交的节点，此时这个节点距离环开始节点的长度和链表投距离环开始的节点的长度相等
+     * @param head
+     * @return
+     */
+    public static Node getFirstNodeInCycle(Node head){
+        Node fast=head,slow = head;
+        while(fast != null && slow != null){
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+            if(fast == slow){
+                break;
+            }
+        }
+        if(fast == null || fast.getNext() == null) return null;//判断是否有环
+        //相遇节点距离环开始节点的长度和链表投距离环开始的节点的长度相等
+        slow=head;
+        while(slow!=fast){
+            slow=slow.getNext();
+            fast=fast.getNext();
+        }//同步走
+        return slow;
 
     }
 
+    /**
+     * 删除delete节点，时间复杂度O(1)
+     * @param head
+     * @param delete
+     * 可采用将delete节点value值与它下个节点的值互换的方法，但是如果delete是最后一个节点，
+     * 则不行，但是总得复杂度还是O(1)
+     */
+    public static void deleteNode(Node head,Node delete){
+        if(delete == null) return;
+        if(delete.getNext() == null){
+            if(head == delete ) head = null;
+            else{
+                Node tmp = head;
+                while(tmp.getNext() != delete){
+                    tmp = tmp.getNext();
+                }
+                tmp.setNext(null);
+            }
+        }else{
+            delete.setValue(delete.getNext().getValue());
+            delete.setNext(delete.getNext().getNext());
+        }
+    }
 
     public static void main(String[] args) {
         //Node head = createLinkedList();
@@ -207,18 +393,40 @@ public class SingleLinkedList {
         //printListReverse(head);
         //printListReverseRec(head);
 
-        Node left9 = new Node(10,null);
-        Node left8  = new Node(9,left9);
-        Node left7  = new Node(8,left8);
-        Node left6  = new Node(7,left8);
-        Node left5  = new Node(6,left6);
-        Node left4  = new Node(5,left7);
-        Node left3  = new Node(4,left4);
-        Node left2  = new Node(3,left5);
-        Node head2  = new Node(2,left2);
-        Node head1 = new Node(1,left3);
-        mergeSortedList(head1,head2);
-        //mergeSortedListRec(head1,head2);
+//        Node left7  = new Node(8,null);
+//        Node left4  = new Node(5,left7);
+//        Node left3  = new Node(4,left4);
+//        Node head1 = new Node(1,left3);
+//
+//        Node left9 = new Node(10,null);
+//        Node left8  = new Node(9,left7);
+//        Node left6  = new Node(7,left8);
+//        Node left5  = new Node(6,left6);
+//        Node left2  = new Node(3,left5);
+//        Node head2  = new Node(2,left2);
+        //mergeSortedList(head1,head2);
+        //printList(mergeSortedListRec(head1,head2));
+
+        Node left8  = new Node(9,null);
+        Node left9 = new Node(10,left8);
+        Node left2  = new Node(3,left9);
+        Node left1  = new Node(2,left2);
+        Node left3  = new Node(4,left1);
+        Node left4  = new Node(5,left3);
+        Node left5  = new Node(6,left4);
+        Node left7  = new Node(8,left5);
+        Node left6  = new Node(7,left7);
+        Node head  = new Node(1,left6);
+//        printList(head);
+        //printList(listSort(head));
+        //printList(insertionSortList(head));
+        //hasCycle(head);
+        //
+        // isIntersect(head1,head2);
+        //System.out.println(getFirstNodeInCycleHashMap(head).getValue());
+        printList(head);
+        deleteNode(head,left9);
+        printList(head);
     }
 
 
